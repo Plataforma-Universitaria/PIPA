21229aaf157aa6a793e7571d0cbc3f71ccf0b0f2
S4R4GDL <saragdl7321@gmail.com>
2025-06-06 15:45:20 -0300

    Ajustando anotação dos repos e readme

--- src/main/java/br/ueg/tc/pipa/domain/accesdata/AccessDataRepository.java
+++ src/main/java/br/ueg/tc/pipa/domain/accesdata/AccessDataRepository.java
@@ -1,6 +1,8 @@
 package br.ueg.tc.pipa.domain.accesdata;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccessDataRepository extends JpaRepository<AccessData,Long> {
 }
--- src/main/java/br/ueg/tc/pipa/domain/institution/InstitutionRepository.java
+++ src/main/java/br/ueg/tc/pipa/domain/institution/InstitutionRepository.java
@@ -1,10 +1,11 @@
 package br.ueg.tc.pipa.domain.institution;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface InstitutionRepository extends JpaRepository<Institution, Long> {
     Optional<Institution> findByShortNameIgnoreCase(String institutionName);
 }
