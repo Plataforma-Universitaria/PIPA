081945f1f44e3096ef78ae8ab718a3ae17ee9880
S4R4GDL <saragdl7321@gmail.com>
2025-08-19 10:59:38 -0300

    FIX - Ajustando propriedades e injeção de email

--- src/main/java/br/ueg/tc/pipa/PipaApplication.java
+++ src/main/java/br/ueg/tc/pipa/PipaApplication.java
@@ -2,8 +2,14 @@ package br.ueg.tc.pipa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = "br.ueg.tc")
+@SpringBootApplication
+@ComponentScan(basePackages = {
+        "br.ueg.tc.pipa_integrator",
+        "br.ueg.tc.ueg_provider",
+        "br.ueg.tc"
+})
 public class PipaApplication {
 
     public static void main(String[] args) {
